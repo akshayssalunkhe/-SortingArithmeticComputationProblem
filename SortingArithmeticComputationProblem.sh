f229e6f96fee1ed4ac9c93c9eeb0c84e5f362a68
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-10 17:49:22 +0530

    ADDED: Input From User

--- SortingArithmeticComputationProblem.sh
+++ SortingArithmeticComputationProblem.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
 
 echo "Welcome To Sorting Arithmetic Computation Problem"
+
+#TAKING THREE INPUTS FROM USER
+read -p "Enter Value Of a: " a
+read -p "Enter Value Of b: " b
+read -p "Enter Value Of c: " c
